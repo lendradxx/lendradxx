61ba81d1f40f16f25653bb0dec1d6c72a19b9fe6
Lendra <lendev0406@gmail.com>
2022-02-22 13:46:51 +0700

    ntalah

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import headerBar from "./components/navbar";
+
+export default function App() {
+    return (
+        <React.StrictMode>
+            <headerBar />
+        </React.StrictMode>
+    )
+}
